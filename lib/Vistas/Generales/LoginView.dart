2aab26f9f907f4065d9517281246d4f13d2cab28
cuatedani <cuate.danielito@gmail.com>
2023-11-28 00:04:52 -0700

    Daniel - Terminado LoginView

--- lib/Vistas/Generales/LoginView.dart
+++ lib/Vistas/Generales/LoginView.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:gps_baby_care/Controladores/usuarioController.dart';
-import 'package:gps_baby_care/Modelos/usuarioModel.dart';
 import 'package:gps_baby_care/Controladores/profesionalController.dart';
+import 'package:gps_baby_care/Modelos/usuarioModel.dart';
 import 'package:gps_baby_care/Modelos/profesionalModel.dart';
 import 'package:gps_baby_care/Vistas/Profesional/MenuProffView.dart';
-import 'RegistroView.dart';
-import '../Usuario/MenuPrincipalView.dart';
-import 'package:gps_baby_care/Vistas/Usuario/ProfesionalesView.dart';
+import 'package:gps_baby_care/Vistas/Generales/RegistroView.dart';
+import 'package:gps_baby_care/Vistas/Usuario/MenuPrincipalView.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -30,279 +29,164 @@ class _LoginViewState extends State<LoginView> {
     return Scaffold(
       body: SingleChildScrollView(
         child: Stack(
-        clipBehavior: Clip.none,
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-          ),
-          Positioned(
-            top: 0,
-            left: 0,
-            right: 0,
-            bottom: MediaQuery.of(context).size.height * 0.55,
-            child: Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/background2.png'),
-                  fit: BoxFit.cover,
-                ),
-              ),
+          clipBehavior: Clip.none,
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
             ),
-          ),
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.38,
-            left: 0,
-            right: 0,
-            bottom: 0,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color(0xFFFAF2E7),
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(50.0),
-                    topRight: Radius.circular(50.0)
-                ),),
-              padding: EdgeInsets.symmetric(horizontal: 30),
-              child: SingleChildScrollView(
-                child: Form(
-                  key: _formKey,
-                  autovalidateMode: _autovalidateMode,
-                  child: Column(
-                    children: <Widget>[
-                      SizedBox(height: 90),
-                      TextFormField(
-                        enableInteractiveSelection: false,
-                        decoration: InputDecoration(
-                          labelText: "Correo electrónico",
-                          suffixIcon: Icon(Icons.verified_user),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                        controller: email,
-                        validator: (value) {
-                          // Implementa tu validación personalizada aquí
-                        },
-                      ),
-                      SizedBox(height: 15),
-                      TextFormField(
-                        enableInteractiveSelection: false,
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          labelText: "Contraseña",
-                          suffixIcon: Icon(Icons.verified_user),
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                        ),
-                        controller: password,
-                        validator: (value) {
-                        },
-                      ),
-                      SizedBox(height: 15,),
-                      ElevatedButton(
-                        onPressed: () async {
-                          bool isEmailValid =
-                          await UsuarioController.verifEmailUsuario(
-                              email.text);
-                          bool isPassValid = await UsuarioController.authUsuario(
-                              email.text, password.text);
-
-                          setState(() {
-                            validEmail = isEmailValid;
-                            validPass = isPassValid;
-                            _autovalidateMode =
-                                AutovalidateMode.onUserInteraction;
-                          });
-
-                          if (_formKey.currentState!.validate()) {
-                            FuncLogin();
-                          }
-                        },
-                        style: ButtonStyle(
-                            fixedSize: MaterialStateProperty.all<Size>(Size(300, 45)),
-                          backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.brown),
-                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(25.0), // Ajusta el radio según sea necesario
-                            ),
-                        ),),
-                        child: Text(
-                          "Ingresar",
-                          style:
-                          TextStyle(fontFamily: 'NerkoOne', fontSize: 20),
-                        ),
-                      ),
-                      SizedBox(height: 40),
-
-                      Text(
-                        "¿Aún no estás registrado?",
-                        style: TextStyle(fontSize: 17),
-                      ),
-                      SizedBox(height: 15),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => RegistroView()),
-                          );
-                        },
-                        style: ButtonStyle(
-                          fixedSize: MaterialStateProperty.all<Size>(Size(300, 40)),
-                          backgroundColor:
-                          MaterialStateProperty.all<Color>(Colors.brown),
-                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(25.0), // Ajusta el radio según sea necesario
-                            ),
-                          ),
-                        ),
-                        child: Text(
-                          "Registrarse",
-                          style:
-                          TextStyle(fontFamily: 'NerkoOne', fontSize: 20),
-                        ),
-                      ),
-                    ],
+            Positioned(
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: MediaQuery.of(context).size.height * 0.55,
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/background2.png'),
+                    fit: BoxFit.cover,
                   ),
                 ),
               ),
             ),
-          ),
-          // Imagen en la mitad superior
-          Positioned(
-            top: MediaQuery.of(context).size.height * 0.25,
-            left: 0,
-            right: 0,
-            child: Container(
-              child: Center(
-                child: Image.asset(
-                  "assets/images/logonobg.png",
-                  width: MediaQuery.of(context).size.width / 2,
-                  height: 200,
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.38,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xFFFAF2E7),
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50.0),
+                      topRight: Radius.circular(50.0)),
                 ),
-              ),
-            ),
-          ),
-        ],
-      ),),
-    );
-  }
-
-
-
-  /*Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Stack(
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage('assets/images/background2.png'),
-                  fit: BoxFit.cover,
-                ),
-              ),
-              height: 200,
-            ),
-            Container(
-              color: Color(0xFFFAF2E7),
-              padding: EdgeInsets.symmetric(horizontal: 30, vertical: 90),
-              child: Form(
-                key: _formKey,
-                autovalidateMode: _autovalidateMode,
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(height: 100),
-                    Center(
-                      child: Image.asset(
-                        "assets/images/logonobg.png",
-                        width: MediaQuery.of(context).size.width / 2,
-                        height: 200,
-                      ),
-                    ),
-                    SizedBox(height: 15),
-                    TextFormField(
-                      enableInteractiveSelection: false,
-                      decoration: InputDecoration(
-                        labelText: "Correo electrónico",
-                        suffixIcon: Icon(Icons.verified_user),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
+                padding: EdgeInsets.symmetric(horizontal: 30),
+                child: SingleChildScrollView(
+                  child: Form(
+                    key: _formKey,
+                    autovalidateMode: _autovalidateMode,
+                    child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 90),
+                        TextFormField(
+                          enableInteractiveSelection: false,
+                          decoration: InputDecoration(
+                            labelText: "Correo electrónico",
+                            suffixIcon: Icon(Icons.verified_user),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          controller: email,
+                          validator: validateEmail,
                         ),
-                      ),
-                      controller: email,
-                      validator: validateEmail,
-                    ),
-                    SizedBox(height: 15),
-                    TextFormField(
-                      enableInteractiveSelection: false,
-                      obscureText: true,
-                      decoration: InputDecoration(
-                        labelText: "Contraseña",
-                        suffixIcon: Icon(Icons.verified_user),
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(20),
+                        SizedBox(height: 15),
+                        TextFormField(
+                          enableInteractiveSelection: false,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                            labelText: "Contraseña",
+                            suffixIcon: Icon(Icons.verified_user),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                          ),
+                          controller: password,
+                          validator: validatePassword,
                         ),
-                      ),
-                      controller: password,
-                      validator: validatePassword,
-                    ),
-                    Divider(height: 15),
-                    ElevatedButton(
-                      onPressed: () async {
-                        bool isEmailValid =
+                        SizedBox(
+                          height: 15,
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            bool isEmailValid =
                             await UsuarioController.verifEmailUsuario(
                                 email.text);
-                        bool isPassValid = await UsuarioController.authUsuario(
-                            email.text, password.text);
+                            bool isPassValid = await UsuarioController.authUsuario(
+                                email.text, password.text);
 
-                        setState(() {
-                          validEmail = isEmailValid;
-                          validPass = isPassValid;
-                          _autovalidateMode =
-                              AutovalidateMode.onUserInteraction;
-                        });
+                            setState(() {
+                              validEmail = isEmailValid;
+                              validPass = isPassValid;
+                              _autovalidateMode =
+                                  AutovalidateMode.onUserInteraction;
+                            });
 
-                        if (_formKey.currentState!.validate()) {
-                          FuncLogin();
-                        }
-                      },
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.brown),
-                      ),
-                      child: Text(
-                        "Ingresar",
-                        style: TextStyle(fontFamily: 'NerkoOne', fontSize: 30),
-                      ),
+                            if (_formKey.currentState!.validate()) {
+                              FuncLogin();
+                            }
+                          },
+                          style: ButtonStyle(
+                            fixedSize:
+                                MaterialStateProperty.all<Size>(Size(300, 45)),
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.brown),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    25.0), // Ajusta el radio según sea necesario
+                              ),
+                            ),
+                          ),
+                          child: Text(
+                            "Ingresar",
+                            style:
+                                TextStyle(fontFamily: 'NerkoOne', fontSize: 20),
+                          ),
+                        ),
+                        SizedBox(height: 40),
+                        Text(
+                          "¿Aún no estás registrado?",
+                          style: TextStyle(fontSize: 17),
+                        ),
+                        SizedBox(height: 15),
+                        ElevatedButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => RegistroView()),
+                            );
+                          },
+                          style: ButtonStyle(
+                            fixedSize:
+                                MaterialStateProperty.all<Size>(Size(300, 40)),
+                            backgroundColor:
+                                MaterialStateProperty.all<Color>(Colors.brown),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(
+                                    25.0), // Ajusta el radio según sea necesario
+                              ),
+                            ),
+                          ),
+                          child: Text(
+                            "Registrarse",
+                            style:
+                                TextStyle(fontFamily: 'NerkoOne', fontSize: 20),
+                          ),
+                        ),
+                      ],
                     ),
-                    SizedBox(height: 20),
-                    Text(
-                      "¿Aún no estás registrado?",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                    SizedBox(height: 5),
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => RegistroView()),
-                        );
-                      },
-                      style: ButtonStyle(
-                        backgroundColor:
-                            MaterialStateProperty.all<Color>(Colors.brown),
-                      ),
-                      child: Text(
-                        "Registrarse",
-                        style: TextStyle(fontFamily: 'NerkoOne', fontSize: 30),
-                      ),
-                    ),
-                  ],
+                  ),
+                ),
+              ),
+            ),
+            // Imagen en la mitad superior
+            Positioned(
+              top: MediaQuery.of(context).size.height * 0.25,
+              left: 0,
+              right: 0,
+              child: Container(
+                child: Center(
+                  child: Image.asset(
+                    "assets/images/logonobg.png",
+                    width: MediaQuery.of(context).size.width / 2,
+                    height: 200,
+                  ),
                 ),
               ),
             ),
@@ -310,7 +194,7 @@ class _LoginViewState extends State<LoginView> {
         ),
       ),
     );
-  }*/
+  }
 
   void FuncLogin() async {
     Usuario User =
